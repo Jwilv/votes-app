12522e3742095e88fcb5104b167570007bdc0016
Jwilv <juan.ignacio.wilvers@gmail.com>
2023-02-03 16:04:21 -0300

    estructura de app

--- votes-app/src/App.jsx
+++ votes-app/src/App.jsx
@@ -2,5 +2,28 @@ import React from "react"
 
 
 export const App = ()=>{
-return <h1>hola</h1>
+return (
+  <div className="container">
+    <div className="alert">
+      <p>
+        Service status:
+        <span className="text-success"> online</span>
+        <span className="text-danger"> offline</span>
+      </p>
+    </div>
+
+    <h1>Band Names</h1>
+    <hr />
+
+<div className="row">
+  <div className="col-8">
+    Band List
+  </div>
+  <div className="col-8">
+    Band Add
+  </div>
+</div>
+
+  </div>
+)
 }
