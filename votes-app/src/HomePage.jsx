3ff2d0a8a7cc68bddb12b2989758d3d0213d2c08
Jwilv <juan.ignacio.wilvers@gmail.com>
2023-02-04 17:44:57 -0300

    se aplico BandChart en HomePage

--- votes-app/src/HomePage.jsx
+++ votes-app/src/HomePage.jsx
@@ -1,5 +1,6 @@
 import React, { useContext } from "react"
 import { BandAdd } from "./components/BandAdd"
+import { BandChart } from "./components/BandChart"
 import { BandList } from "./components/BandList"
 import { SocketContext } from "./context/SocketContext"
 
@@ -24,6 +25,12 @@ export const HomePage = () => {
 
       <h1>Band Names</h1>
       <hr />
+
+      <div className="row">
+        <div className="col">
+          <BandChart />
+        </div>
+      </div>
 
       <div className="row">
         <div className="col-8">
