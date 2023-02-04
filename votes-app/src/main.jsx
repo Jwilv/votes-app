b5226dfb54c5a3180532ae893c476e8f9523ecb4
Jwilv <juan.ignacio.wilvers@gmail.com>
2023-02-04 16:27:40 -0300

    csmbios y aplicacion de SocketProvider

--- votes-app/src/main.jsx
+++ votes-app/src/main.jsx
@@ -1,7 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import {App} from './App'
+import { App } from './App'
+import {BandNamesApp}  from './BandNamesApp'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-    <App />
+    <BandNamesApp />
+    // <App />
 )
