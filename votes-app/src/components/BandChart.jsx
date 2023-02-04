e990ab50dd40d3c6c8435e0ed3013e94a6f7e93d
Jwilv <juan.ignacio.wilvers@gmail.com>
2023-02-04 18:56:42 -0300

    grafico en tiempo real

--- votes-app/src/components/BandChart.jsx
+++ votes-app/src/components/BandChart.jsx
@@ -1,9 +1,66 @@
-import React from 'react'
+//import { Chart } from 'chart.js';
+import React, { useContext, useEffect, useState } from 'react'
+import { Chart, registerables } from 'chart.js/auto';
+import { SocketContext } from '../context/SocketContext';
+Chart.register(...registerables);
 
 export const BandChart = () => {
+
+    const { socket } = useContext(SocketContext)
+
+    var myChart = null;
+
+    useEffect(() => {
+        socket.on('current-bands', (bands) => {
+            createGrafica(bands);
+        })
+
+    }, [socket])
+
+    const createGrafica = (bands = []) => {
+        const ctx = document.getElementById('myChart');
+        if(myChart!=null){
+            myChart.destroy()
+        }
+        myChart = new Chart(ctx, {
+            type: 'bar',
+            data: {
+                labels: [...bands.map(band => band.name)],
+                datasets: [{
+                    label: '# of Votes',
+                    data: bands.map(band => band.votes),
+                    backgroundColor: [
+                        'rgba(255, 99, 132, 0.2)',
+                        'rgba(54, 162, 235, 0.2)',
+                        'rgba(255, 206, 86, 0.2)',
+                        'rgba(75, 192, 192, 0.2)',
+                        'rgba(153, 102, 255, 0.2)',
+                        'rgba(255, 159, 64, 0.2)'
+                    ],
+                    borderColor: [
+                        'rgba(255, 99, 132, 1)',
+                        'rgba(54, 162, 235, 1)',
+                        'rgba(255, 206, 86, 1)',
+                        'rgba(75, 192, 192, 1)',
+                        'rgba(153, 102, 255, 1)',
+                        'rgba(255, 159, 64, 1)'
+                    ],
+                    borderWidth: 1
+                }]
+            },
+            options: {
+                indexAxis: 'y',
+                animation: false,
+                scales: {
+
+                }
+            }
+        });
+
+
+    }
+
     return (
-        <>
-            hola
-        </>
+        <canvas id="myChart"></canvas>
     )
 }
